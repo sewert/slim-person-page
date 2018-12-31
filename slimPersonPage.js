70057d924cf4e788eeecfcb17213c6478d4b4420
Spencer Ewert <sewert@familysearch.org>
2018-12-31 10:23:00 -0700

    Make TF request using pid/session

--- slimPersonPage.js
+++ slimPersonPage.js
@@ -1,4 +1,16 @@
 const urlParams = new URLSearchParams(window.location.search);
 const sessionId = urlParams.get('sessionId');
 const pid = urlParams.get('pid');
-alert('sessionId=' + sessionId + " pid=" + pid);
+
+var tfRequest = new XMLHttpRequest();
+tfRequest.open('GET', 'https://integration.familysearch.org/tf/person/' + pid + '?oneHops=cards&includeSuggestions=true&contactNames=true&sessionId=' + sessionId, true);
+tfRequest.send();
+tfRequest.addEventListener('readystatechange', processTfRequest, false);
+
+function processTfRequest() {
+  if (tfRequest.readyState === 4 && tfRequest.status === 200) { // if DONE
+    var response = JSON.parse(tfRequest.responseText);
+    var body = document.getElementById('bodyId');
+    body.innerText = JSON.stringify(response);
+  }
+}
