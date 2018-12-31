35ef3988e1dfcf485fee7acbe3d252132d0546e9
Spencer Ewert <sewert@familysearch.org>
2018-12-31 09:35:25 -0700

    Return html/css/js files
    For demo purposes, I'll have the browser make all the requests so the node app will simply just return 1 file each for html/css/js

--- server.js
+++ server.js
@@ -1,16 +1,31 @@
 const http = require('http');
-const url = require('url');
+const fs = require('fs');
 
 const hostname = 'localhost';
 const port = 8080;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  var urlparsed = url.parse(req.url, true);
-  var sessionId= urlparsed.query.sessionId;
-  var personId = urlparsed.pathname;
-  res.write('personId=' + personId + ' sessionId=' + sessionId);
-  res.end();
+const server = http.createServer(function (req, res) {
+  if (req.url.indexOf('.js') !== -1) { // going to just return one js file
+    fs.readFile('slimPersonPage.js', function(err, data) {
+      res.writeHead(200, {'Content-Type': 'text/javascript'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else if (req.url.indexOf('.css') !== -1) {
+    fs.readFile('slimPersonPage.css', function(err, data) { // going to just return one css file
+      res.writeHead(200, {'Content-Type': 'text/css'});
+      res.write(data);
+      res.end();
+    });
+  }
+  else {
+    fs.readFile('slimPersonPage.html', function(err, data) { // going to just return one html file
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write(data);
+      res.end();
+    });
+  }
 });
 
 server.listen(port, hostname, () => {
